f1a1c002669eeb1075ac553224065e9531b63979
NguyenHaiLuan <nguyenhailuan609@gmail.com>
2025-01-23 15:45:39 +0700

    call.dart and assets for Call Screen

---
+++ lib/ui/call_screen/call.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CallScreen extends StatefulWidget {
+  const CallScreen({super.key});
+
+  @override
+  _CallScreenState createState() => _CallScreenState();
+}
+
+class _CallScreenState extends State<CallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: SafeArea(
+        child: Column(),
+      ),
+    );
+  }
+}
